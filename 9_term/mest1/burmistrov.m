b50f1fa27d135ab6eae850fe4e700b43e88f43b8
burmisha <burmisha@gmail.com>
2012-12-04 15:13:30 +0400

    v9.1

--- 9_term/mest1/burmistrov.m
+++ 9_term/mest1/burmistrov.m
@@ -1,12 +1,10 @@
 clear all;
 
-FileNumber = 5;
-FileNamePrefix = sprintf('%d',FileNumber);
+FileNumber = 2;
 RadiusOptimal = [500, 12, 13, 13, 19];
-X = dlmread(strcat(FileNamePrefix,'.txt'));
+X = dlmread(sprintf('%d.txt',FileNumber));
 %X = [X(:,1), X(:,3)];  % 2 -> 12 .. 10     % 3 -> 13 .. 12     % 4 -> 13 .. 11     % 5 -> 19 only
 r = RadiusOptimal(FileNumber);
-r = 13;
 MinAngle = -179;
 
 AngleIdxInit = @() ndgrid(1000, 0);
@@ -15,11 +13,7 @@ clangle = @(x) (1.25-4*(heaviside(x(:,1).*x(:,4)-x(:,2).*x(:,3))-0.75).^2) .*aco
 h = plot(X(:,1), X(:,2), 'r.','LineWidth',1);
 axis normal; hold on;
 
-max(X)
-%[~, idx] = max(X(:,1)==max(X(:,1)));
-MaxedX=find(X(:,1)==max(X(:,1)));
-[~, idx] = max(X(MaxedX, 2));
-idx = MaxedX(idx);
+[~, idx] = max(X(:,1)==max(X(:,1)));
 plot(X(idx,1), X(idx,2), 'b.','LineWidth',3);
 
 Previous = X(idx,:);
@@ -31,7 +25,7 @@ Angle = clangle([ones(length(nearest),1) * [0 -1] (XX - ones(length(nearest),1) 
 [~, NewIdx] = min(Angle(Angle > MinAngle));
 idx = [idx nearest(NewIdx)];
 plot(X(idx(end),1), X(idx(end),2), 'b.','LineWidth',3);
-X(idx,:)
+
 while idx(end) ~= idx(1)
     Previous = X(idx(end),:);
     PrePrevious = X(idx(end-1),:);
@@ -49,11 +43,11 @@ while idx(end) ~= idx(1)
     else
         Negative = find((Angle > MinAngle) & (NPrev > NPPrev));
         [~, BestNeg] = min(Angle(Negative));
-        NewIdx = nearest(Negative(BestNeg))
+        NewIdx = nearest(Negative(BestNeg));
     end
-    plot(XX(Positive,1), XX(Positive,2), 'y.','LineWidth',3);
+    %plot(XX(Positive,1), XX(Positive,2), 'y.','LineWidth',3);
     idx = [idx NewIdx];
-    plot(XX(Positive,1), XX(Positive,2), 'r.','LineWidth',3);
+    %plot(XX(Positive,1), XX(Positive,2), 'r.','LineWidth',3);
     plot(X(idx(end),1), X(idx(end),2), 'g.','LineWidth',3);
     %length(idx)
 end
@@ -66,7 +60,9 @@ plot(X(idx,1), X(idx,2), 'g-','LineWidth',2);
 axis('square');
 axis('xy');
 text(   0.5*min(X(:,1)) + 0.5* max(X(:,1)), min(X(:,2)), ...
-    strcat('$$P = ', num2str(Perimeter),', S = ',num2str(Area),'$$'), ...
-    'Interpreter','latex', 'FontSize',12);
-saveas(h, strcat(FileNamePrefix, sprintf('_r%d.eps',r)),        'eps2c');
+        strcat('$$P = ', num2str(Perimeter),', S = ',num2str(Area),'$$'), ...
+        'Interpreter','latex', 'FontSize',12);
+saveas(h, sprintf('%d_r%d.eps',FileNumber,r),        'eps2c');
 hold off
+
+
