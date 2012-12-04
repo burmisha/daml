743a1fce14520596ab0a061d37eee7e6e8757d87
burmisha <burmisha@gmail.com>
2012-12-04 15:12:41 +0400

    v7

--- 9_term/mest1/burmistrov.m
+++ 9_term/mest1/burmistrov.m
@@ -2,15 +2,15 @@ clear all;
 
 X = dlmread('2.txt', ' ', 0, 0);
 X = [X(:,1), X(:,3)];
-% 2 -> 12
-% 3 -> 13
-% 4 -> 13
-% 5 -> 19
+% 2 -> 12 .. 10
+% 3 -> 13 .. 12
+% 4 -> 13 .. 11
+% 5 -> 19 only
 r = 12;
-MinAngle = -178;
+MinAngle = -179;
 
 AngleIdxInit = @() ndgrid(1000, 0);
-clangle = @(u,v) (1.25-4*(heaviside((u(1)*v(2)-u(2)*v(1)))-0.75)^2)* acos(dot(u,v)/(norm(u)*norm(v)))*180/pi;
+clangle = @(x) (1.25-4*(heaviside(x(:,1).*x(:,4)-x(:,2).*x(:,3))-0.75).^2) .*acos((x(:,1).*x(:,3)+x(:,2).*x(:,4))./sqrt((x(:,1).^2+ x(:,2).^2) .*(x(:,3).^2+ x(:,4).^2)))*180/pi;
 
 plot(X(:,1), X(:,2), 'r.','LineWidth',1);
 axis normal; hold on;
@@ -22,7 +22,7 @@ plot(X(idx,1), X(idx,2), 'b.','LineWidth',3);
 for i=1:size(X,1)
     Dist = norm(X(i,:) - X(idx,:));
     if (Dist < r) && (i ~= idx)
-        Angle = clangle([0 -1], X(i,:)- X(idx,:));
+        Angle = clangle([0 -1 (X(i,:)-X(idx,:))]);
         if (Angle > MinAngle) && (BestAngle > Angle)
             BestIdx = i;
             BestAngle = Angle;
@@ -40,20 +40,20 @@ while idx(end) ~= idx(1)
     [TestAngle, TestIdx] = AngleIdxInit();
     nearest = find(((X(:,1) - Previous(1)).^2 + (X(:,2) - Previous(2)).^2) < r^2);
     nearest(nearest == idx(end)) = [];
-    for i=nearest'
-        Angle = clangle(LastLine, X(i,:)- Previous);
-        if (Angle > MinAngle)
-            NPrev = norm(X(i,:) - Previous);
-            NPPrev = norm(X(i,:) - PrePrevious);
-            if NPrev <= NPPrev
-                if (BestAngle > Angle)
-                    BestIdx = i;
-                    BestAngle = Angle;
-                end
-                if (TestAngle > Angle)
-                    TestIdx = i;
-                    TestAngle = Angle;
-                end
+    XX = X(nearest,:);
+    NPrev = (XX(:,1) - Previous(1)).^2 + (XX(:,2) - Previous(2)).^2;
+    NPPrev = (XX(:,1) - PrePrevious(1)).^2 + (XX(:,2) - PrePrevious(2)).^2;
+    Angle = clangle([ones(length(nearest),1) * LastLine (XX-ones(length(nearest),1)*Previous) ] );
+    for i=find(Angle > MinAngle)'
+        if NPrev(i) <= NPPrev(i)
+            if (BestAngle > Angle(i))
+                BestIdx = nearest(i);
+                BestAngle = Angle(i);
+            end
+        else
+            if (TestAngle > Angle(i))
+                TestIdx = nearest(i);
+                TestAngle = Angle(i);
             end
         end
     end
@@ -62,7 +62,7 @@ while idx(end) ~= idx(1)
     end
     idx = [idx BestIdx];
     %plot(X(BestIdx,1), X(BestIdx,2), 'g.','LineWidth',3);
-    length(idx)
+    %length(idx)
 end
 
 plot(X(idx,1), X(idx,2), 'g-','LineWidth',2);
