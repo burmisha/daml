ecc8c37b66fd3480198c2ca9f8b1c77255da4283
burmisha <burmisha@gmail.com>
2012-12-04 21:45:28 +0400

    build linear regression on contours

--- 9_term/mest2/burm2012fishbird.m
+++ 9_term/mest2/burm2012fishbird.m
@@ -1,13 +1,28 @@
 clear all;
-fish_fileidx = 1:55;
-bird_fileidx = 1:52;
+fish_fileidx = 1:55; % - 0
+bird_fileidx = 1:52; % - 1
 
 % 15 - good, 10 - too
-collect(fish_fileidx, 'fish')
-collect(bird_fileidx, 'bird')
+[FP, FA, FI, FPc, FAc, FIc] = collect(fish_fileidx, 'fish');
+[BP, BA, BI, BPc, BAc, BIc] = collect(bird_fileidx, 'bird');
 hold off
-%plot(F_PerAr(:,1).^2, F_PerAr(:,2), 'r.'); hold on
-%plot(B_PerAr(:,1).^2, B_PerAr(:,2), 'b.'); hold on
+plot(FP.^2, FA, 'r.'); hold on
+plot(BP.^2, BA, 'b.'); hold on
+
+hold off
+plot(FI, FIc, 'r.'); hold on
+plot(BI, BIc, 'b.'); hold on
+
+hold off
+plot(FAc, FA, 'r.'); hold on
+plot(BAc, BA, 'b.'); hold on
+
+%% 
+Matrix = [[FP, FA, FI, FPc, FAc, FIc]; [BP, BA, BI, BPc, BAc, BIc]];
+Answers = [zeros(length(fish_fileidx), 1); ones(length(bird_fileidx), 1)];
+y = regress(Answers, Matrix);
+sum(abs(round(Matrix*y) - Answers))
+
 %%
 X = read_cloud('bird', 27);
 B = binarise_cloud(X, 10);
--- 9_term/mest2/collect.m
+++ 9_term/mest2/collect.m
@@ -1,21 +1,23 @@
-function [ Per, Area,IdSize,ConvSize ] = collect( fileidx, filename_prefix )
+function [ Per, Area,Id,ConvPer,ConvArea,ConvId ] = collect( fileidx, filename_prefix )
 %COLLECT Summary of this function goes here
 %   Detailed explanation goes here
     R = 10;
     R_bin = 4;
     Per=zeros(length(fileidx), 1);
     Area=zeros(length(fileidx), 1);
-    IdSize=zeros(length(fileidx), 1);
-    ConvSize=zeros(length(fileidx), 1);
+    Id=zeros(length(fileidx), 1);
+    ConvPer=zeros(length(fileidx), 1);
+    ConvArea=zeros(length(fileidx), 1);
+    ConvId=zeros(length(fileidx), 1); 
     for number=fileidx
         X = read_cloud(filename_prefix, number);
         [Y, Side] = norm_to_square(X);
         [idx, Per(number), Area(number)]  = get_contour(Y, R);
-        IdSize(number) = length(idx);
-        ConvSize(number) = length(get_contour(Y, Side));
+        Id(number) = length(idx);
+        [idxConv, ConvPer(number), ConvArea(number)]  = get_contour(Y, 2*Side);
+        ConvId(number) = length(idxConv);
+        % plot_contour(X, idx);
         % imshow(binarise_cloud(X, R_bin)');
-        plot_contour(X, idx);
-        ConvSize(number) = length(get_contour(Y, Side));
         number
     end
 end
