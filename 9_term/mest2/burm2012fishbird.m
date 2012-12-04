2171a2d6a5a01efdf5706cee5577552dd73ad725
burmisha <burmisha@gmail.com>
2012-12-04 19:58:27 +0400

    optimise perimeter and area

--- 9_term/mest2/burm2012fishbird.m
+++ 9_term/mest2/burm2012fishbird.m
@@ -1,39 +1,21 @@
 clear all;
-folder_name = 'data';
-fish_prefix = 'fish (';
-bird_prefix = 'bird (';
-suffix = ').txt';
 fish_fileidx = 1:55;
 bird_fileidx = 1:52;
-number = 1;
 
-% R for fish 15
-% R for bird 15
-% only 15
 % 15 - good, 10 - too
 R = 10;
-F_p = [];
-F_a = [];
+F_PerAr=zeros(length(fish_fileidx), 2);
 for number=fish_fileidx
     X = read_cloud('fish', number);
-    [idx, P, A]  = get_contour(norm_to_square(X),R);
-    F_p = [F_p P];
-    F_a = [F_a A];
+    [idx, F_PerAr(number,1 ), F_PerAr(number, 2)]  = get_contour(norm_to_square(X),R);
     % plot_contour(X, idx);
-    number
 end
-
-
-B_a = [];
-B_p = [];
+B_PerAr=zeros(length(bird_fileidx), 2);
 for number=bird_fileidx
     X = read_cloud('bird', number);
-    [idx, P, A]  = get_contour(norm_to_square(X),R);
-    B_p = [B_p P];
-    B_a = [B_a A];
+    [idx, B_PerAr(number,1 ), B_PerAr(number, 2)]  = get_contour(norm_to_square(X),R);
     % plot_contour(X, idx);
-    number
 end
 hold off
-plot(F_p.^2, F_a, 'r.'); hold on
-plot(B_p.^2, B_a, 'b.'); hold on
+%plot(F_PerAr(:,1).^2, F_PerAr(:,2), 'r.'); hold on
+%plot(B_PerAr(:,1).^2, B_PerAr(:,2), 'b.'); hold on
