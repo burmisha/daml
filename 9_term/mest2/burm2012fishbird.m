ebf5485678f6db9fb5f1c2a00ee2a2b4670ec6b7
burmisha <burmisha@gmail.com>
2012-12-04 15:49:11 +0400

    old_filenames

---
+++ 9_term/mest2/burm2012fishbird.m
@@ -0,0 +1,10 @@
+clear all;
+folder_name = 'data';
+fish_prefix = '?';
+bird_prefix = '?'
+fish_fileidx = 1:52;
+bird_fileidx = [1:47 49:56];
+% ????: 
+number = 5
+strcat(fish_prefix)
+file_name = strcat(folder_name, '//', fish_prefix, '_', num2str(number), 'txt')
