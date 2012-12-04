c26b392c10844924affae60f3620b37b928fd784
burmisha <burmisha@gmail.com>
2012-12-04 15:11:23 +0400

    v2

--- 9_term/mest1/burmistrov.m
+++ 9_term/mest1/burmistrov.m
@@ -1,22 +1,22 @@
+clear all;
 %close
 X = dlmread('input.txt', ' ', 1, 0);
 X=[X(:,1), X(:,2)]
 r = 1.2;
 %%
-X = dlmread('2.txt', ' ', 0, 0);
+X = dlmread('3.txt', ' ', 0, 0);
 X=[X(:,1), X(:,3)];
-% 2 - 16-ok
-r = 15;
+% 2 -> 16, -150
+% 3 -> 27, -150
+% 4 -> 13, -150
+r = 13;
+MinAngle = -150;
 
 %%
-%max_coords = max(X,[],1);
-%max_x = max_coords(1);
-[~, first_idx] = max(X(:,1)==max(X(:,1)));
-%h = figure;
-plot(X(:,1), X(:,2), 'r.','LineWidth',2);
-axis normal
-hold on
+plot(X(:,1), X(:,2), 'r.','LineWidth',1); 
+axis normal; hold on;
 
+[~, first_idx] = max(X(:,1)==max(X(:,1)));
 idx = first_idx;
 plot(X(first_idx,1), X(first_idx,2), 'b.','LineWidth',3);
 
@@ -38,34 +38,38 @@ while idx(end) ~= first_idx
     Previous = X(idx(end),:);
     PrePrevious = X(idx(end-1),:);
     LastLine = Previous - PrePrevious;
-    tmp = [];
+    BestAngle = 1000;
+    BestDist = 2*r;
+    BestIdx = 0;
+    TestIdx = BestAngle;
+    TestAngle = BestAngle;
     for i=1:size(X,1)
         Dist = norm(X(i,:) - Previous);
-        Angle = clangle(LastLine, X(i,:)- Previous);
-        %if (dist < r) && (i ~= idx(end)) && (ang > -120)
-        if (Dist < r) && (i ~= idx(end)) && (Angle > -150) ...
-            && (norm(X(i,:) - Previous) <= norm(X(i,:) - PrePrevious)) 
-            tmp = [tmp; i Angle Dist];
+        if (Dist < r) && (i ~= idx(end)) 
+            Angle = clangle(LastLine, X(i,:)- Previous);
+            %if ((Angle > MinAngle) && (norm(X(i,:) - Previous) <= norm(X(i,:) - PrePrevious))) ...
+            if (Angle > MinAngle) && (norm(X(i,:) - Previous) <= norm(X(i,:) - PrePrevious)) ...
+                    && ((BestAngle > Angle) || (BestAngle == Angle) && (Dist > BestDist))
+                BestIdx = i;
+                BestAngle = Angle;
+                BestDist = Dist;
+            end
+            if (Angle > MinAngle) && (norm(X(i,:) - Previous) > norm(X(i,:) - PrePrevious)) ...
+                    && (TestAngle > Angle)
+                TestIdx = i;
+                TestAngle = Angle;
+            end
         end        
     end
-    %[X(tmp(:,1),:), tmp(:, 2:3)]
-    if (isempty(tmp))
+    if (BestIdx == 0)
         error='ERROR! no points'
-        break;
+        % break;
+        BestIdx = TestIdx;
     end
-    [~, ia] = min(tmp(:,2)+400);
-    ba = tmp(ia,:);
-    [~, id] = max(ba(:, 3));
-    BestIdx = ba(id, 1);
-    plot(X(BestIdx,1), X(BestIdx,2), 'g.','LineWidth',3);
     idx = [idx BestIdx];
-    % X(best_idx,:);
-    %pause(0.1)
+    plot(X(BestIdx,1), X(BestIdx,2), 'g.','LineWidth',3);
     length(idx)
 end
 
 plot(X(idx,1), X(idx,2), 'g-','LineWidth',1);
-
 hold off
-X(idx,:);
-%sortrows([5 2; 3 4], 2);
