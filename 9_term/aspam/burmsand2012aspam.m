2ab0609764e3816b3020086fdffa3b8f7ea114d3
burmisha <burmisha@gmail.com>
2012-12-22 19:28:43 +0400

    compare real and predicted

--- 9_term/aspam/burmsand2012aspam.m
+++ 9_term/aspam/burmsand2012aspam.m
@@ -1,7 +1,17 @@
 clear all
-X = [(randn(10,2)+ones(10,2)*(-10)); randn(200,2)];
+% X = [(randn(10,2)+ones(10,2)*(-10)); randn(200,2)];
 
-model = svdd(X, 1.2);
+N = 400;
+dim = 2;
+R = 5;
+c = 0.05;
+a=[1;2];
+
+C = 0.022;
+
+X = get_data(N, dim, a, R, c);
+
+model = svdd(X, C);
 hold off
 Plot = plot(X(:,1),X(:,2), 'r.', 'LineWidth',3); axis equal; hold on
 
@@ -11,9 +21,11 @@ plot(model.in(:,1), model.in(:,2),  '.','Color',[0.9 0 0.1],'LineWidth',2)
 
 mean(sqrt(sum((model.on - repmat(model.center', size(model.on, 1),1)).^2,2)));
 
-a = model.center;
-R = model.radius+10^-9;
-plot(a(1), a(2), '.', 'Color',[220 150 90]/255, 'LineWidth',3)
-rectangle('Position',[a(1)-R, a(2)-R, 2*R, 2*R],'Curvature',[1,1],'EdgeColor','g')
+a_predicted = model.center
+R_predicted = model.radius+10^-9
+plot(a_predicted(1), a_predicted(2), '.', 'Color',[220 150 90]/255, 'LineWidth',3)
+rectangle('Position',[a_predicted(1)-R_predicted, a_predicted(2)-R_predicted,...
+    2*R_predicted, 2*R_predicted],'Curvature',[1,1],'EdgeColor','g'); hold on
+rectangle('Position',[a(1)-R, a(2)-R, 2*R, 2*R],'Curvature',[1,1],'EdgeColor','r')
 
 saveas(Plot, strcat('modelData','.eps'), 'eps2c');
