1af08f20ee53edc77e1cd7e95eb6608ef9a826fa
burmisha <burmisha@gmail.com>
2012-12-25 03:11:48 +0400

    one more plot

--- 9_term/aspam/crossvalidtion.m
+++ 9_term/aspam/crossvalidtion.m
@@ -1,10 +1,10 @@
 clear all
 
-T = 10;
+T = 30;
 Q = 3;
-C = 0.015:0.0005:0.05;
+C = 0.015:0.0005:0.045;
 
-N = 400;
+N = 500;
 dim = 2;
 a = [1;2];
 R = 3;
@@ -46,20 +46,20 @@ for i=1:length(C)
             Precision(q,t) = Pre;
             Recall(q,t) = Rec;
             
-            hold off
-            plot(model.on(:,1), model.on(:,2),  '.','Color',[0.0 1 0.0],'LineWidth',4)
-            hold on
-            plot(model.out(:,1),model.out(:,2), '.','Color',[0.1 0 0.9],'LineWidth',2)
-            plot(model.in(:,1), model.in(:,2),  '.','Color',[0.9 0 0.1],'LineWidth',2)
-
-            a_predicted = model.center;
-            R_predicted = model.radius+10^-9;
-            plot(a_predicted(1), a_predicted(2), '.', 'Color',[220 150 90]/255, 'LineWidth',3)
-            rectangle('Position',[a_predicted(1)-R_predicted, a_predicted(2)-R_predicted,...
-                2*R_predicted, 2*R_predicted],'Curvature',[1,1],'EdgeColor','r'); hold on
-            rectangle('Position',[a(1)-R, a(2)-R, 2*R, 2*R],'Curvature',[1,1],'EdgeColor','g')
-            
-            misses(q,t) = sum(sqrt(sum((X_test - ones(size(X_test,1),1)*model.center').^2,2)) > model.radius) / size(X_test,1);
+%             hold off
+%             plot(model.on(:,1), model.on(:,2),  '.','Color',[0.0 1 0.0],'LineWidth',4)
+%             hold on
+%             plot(model.out(:,1),model.out(:,2), '.','Color',[0.1 0 0.9],'LineWidth',2)
+%             plot(model.in(:,1), model.in(:,2),  '.','Color',[0.9 0 0.1],'LineWidth',2)
+% 
+%             a_predicted = model.center;
+%             R_predicted = model.radius+10^-9;
+%             plot(a_predicted(1), a_predicted(2), '.', 'Color',[220 150 90]/255, 'LineWidth',3)
+%             rectangle('Position',[a_predicted(1)-R_predicted, a_predicted(2)-R_predicted,...
+%                 2*R_predicted, 2*R_predicted],'Curvature',[1,1],'EdgeColor','r'); hold on
+%             rectangle('Position',[a(1)-R, a(2)-R, 2*R, 2*R],'Curvature',[1,1],'EdgeColor','g')
+%             
+%             misses(q,t) = sum(sqrt(sum((X_test - ones(size(X_test,1),1)*model.center').^2,2)) > model.radius) / size(X_test,1);
         end
     end
     PP(i) = mean(mean(Precision));
@@ -77,5 +77,5 @@ hold off
 Quality;
 p = plot(C, Quality, 'r-');
 name = sprintf('CV_N%d_%0.4f-%0.4f-%0.4f_T%d_Q%d',N,C(1),C(2)-C(1),C(end),T,Q);
-saveas(p, strcat(name,'.jpg'), 'jpg');
+saveas(p, strcat(name,'.png'), 'png');
 saveas(p, strcat(name,'.eps'), 'eps');
