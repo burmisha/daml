a464932df051cfe8d16adb0c2ee1bfe1c0765990
burmisha <burmisha@gmail.com>
2013-01-02 20:54:23 +0400

    full refactoring svdd, chng example, not working

--- 9_term/aspam/example_model.m
+++ 9_term/aspam/example_model.m
@@ -1,15 +1,16 @@
 clear all
 % X = [(randn(10,2)+ones(10,2)*(-10)); randn(200,2)];
 
-N = 240;
+N = 150;
 dim = 2;
-R = 3;
-c = 0.2;
+R = 1.5;
+c = 0.6;
 a = [1;6];
 
 C = 0.008;
 
-X = get_data(N, dim, a, R, c);
+X = [get_data(N, dim, a, R, c); ...
+     get_data(N, dim, a+[5;0], R, c)];
 
 model = svdd(X, C);
 %%
@@ -20,26 +21,15 @@ plot(model.on(:,1), model.on(:,2),  '.','Color',[0.0 1 0.0],'LineWidth',2)
 plot(model.out(:,1),model.out(:,2), '.','Color',[0.1 0 0.9],'LineWidth',2)
 plot(model.in(:,1), model.in(:,2),  '.','Color',[0.9 0 0.1],'LineWidth',2)
 
-mean(sqrt(sum((model.on - repmat(model.center', size(model.on, 1),1)).^2,2)));
+rectangle('Position',[a(1)-R, a(2)-R, 2*R, 2*R],'Curvature',[1,1],'EdgeColor','g','LineWidth',2); hold on
 
-a_predicted = model.center
-R_predicted = model.radius+10^-9
-
-%plot(a_predicted(1), a_predicted(2), '.', 'Color',[220 150 90]/255, 'LineWidth',2)
-rectangle('Position',[a_predicted(1)-R_predicted, a_predicted(2)-R_predicted,...
-    2*R_predicted, 2*R_predicted],'Curvature',[1,1],'EdgeColor','r','LineWidth',2); hold on
-rectangle('Position',[a(1)-R, a(2)-R, 2*R, 2*R],'Curvature',[1,1],'EdgeColor','g','LineWidth',2)
-
-saveas(Plot, strcat('example','.eps'), 'eps2c');
+% saveas(Plot, strcat('example','.eps'), 'eps2c');
 
 save_everything('Example.txt', '_Example',[2,2], X);
-
-model.classify([1, 1])
-
 %%
 
-First = a(1)-2:.03:a(1)+2;
-Second = a(2)-2:.03:a(2)+2;
+First = a(1)-4:.1:a(1)+4;
+Second = a(2)-4:.1:a(2)+4;
 [X1, X2] = meshgrid(First,Second);
 coords = cell(length(First), length(Second));
 for i=1:length(First)
--- 9_term/aspam/svdd.m
+++ 9_term/aspam/svdd.m
@@ -1,104 +1,59 @@
-function [ model ] = svdd(Data, C)
+function [ m ] = svdd(Data, C)
 %SVDD builds Support Vector Data Description model
 %   Mikhail Burmistrov, Liubov Sanduleanu
 %   2012, Moscow, MIPT
 
-    model.Data = Data;
+    m.Data = Data;
+    m.C = C;
     
     kernel = @(u,v) (u*v'); 
-    model.kernel = kernel;
+    % kernel = @(u,v) (exp(norm(u-v,2)/5));
+    m.kernel = kernel;
     % u - 1 object, row; v - matrix (1 row is 1 object)
     % kernel returns a row of results
     
-    %prod = @(u,v) (exp(norm(u-v,2)/5));
-    
     Number = size(Data, 1);
+    if Number * C < 1
+        error( 'ERROR: Too small C. Radius is 0.')
+    end
+    
     epsilon = 10^-16;
-    
-    alpha_Zero = ones(Number,1)/Number;
-    center_Zero = Data'*alpha_Zero;
-    radius_Zero = 0;
-    DistSq_Zero = sum((Data - ones(Number, 1)*center_Zero').^2,2);
-    
-    PairWise = zeros(Number); % count matrix || x_i\cdot x_j ||
+
+    m.PairWise = zeros(Number); % count matrix || x_i\cdot x_j ||
     for i=1:Number
         for j=1:Number
-            PairWise(i,j) = kernel(Data(i,:), Data(j,:));
+            m.PairWise(i,j) = kernel(Data(i,:), Data(j,:));
         end
     end
-    model.PairWise = PairWise;
-    
-    if Number * C < 1
-        alpha = alpha_Zero;
-        center = center_Zero;
-        radius = radius_Zero;
-        
-        model.in_idx  = find(DistSq_Zero <  radius);
-        model.on_idx  = find(DistSq_Zero == radius);
-        model.out_idx = find(DistSq_Zero >  radius);        
-    else   
-        SingleWise = zeros(Number,1); % count vector || x_i\cdot x_i ||
-        for i=1:Number
-            SingleWise(i) = kernel(Data(i,:), Data(i,:));
-        end
 
-        CoeffEq = ones(1,Number);
-        ValEq = 1;
-        lowerBound = zeros(Number,1);   % \alpha_i >= 0
-        upperBound = C*ones(Number,1);  % \alpha_i <= C 
-        opts = optimset('Algorithm','active-set','Display','off','TolX',1.e-15);
-        H = -2 * PairWise;     % cause it minimises 1/2*x'*H*x
-        % see http://www.mathworks.com/help/optim/ug/quadprog.html for full decription    
-        [alpha_NotZ, L_NotZ] = quadprog(-H,-SingleWise,[],[],CoeffEq,ValEq,lowerBound,upperBound,[],opts);
-        L_NotZ = -L_NotZ;
-        center_NotZ = Data'*alpha_NotZ;
-        DistSq_NotZ = sum((Data - ones(Number, 1)*center_NotZ').^2,2);
-        OnIdx_NotZ  = find((alpha_NotZ >= epsilon) & (alpha_NotZ <= C - epsilon));
-        if isempty(OnIdx_NotZ)
-            radius_In  = max(DistSq_NotZ(alpha_NotZ < epsilon));
-            radius_Out = min(DistSq_NotZ(alpha_NotZ > C - epsilon));
-            radius_NotZ = mean(sqrt([radius_In; radius_Out]));
-            
-            model.in_idx  = find(DistSq_NotZ < radius_NotZ - epsilon);
-            model.on_idx  = find(abs(DistSq_NotZ - radius_NotZ) <= epsilon);
-            model.out_idx = find(DistSq_NotZ > radius_NotZ + epsilon);
-        else
-            radius_NotZ = mean(sqrt(sum( (Data(OnIdx_NotZ,:) - ones(length(OnIdx_NotZ),1)*center_NotZ').^2 ,2)));
-
-            model.in_idx = find(abs(alpha_NotZ) < epsilon);
-            model.on_idx = find((alpha_NotZ >= epsilon) & (alpha_NotZ <= C - epsilon));
-            model.out_idx = find(alpha_NotZ > C - epsilon);
-        end
-        
-        L_Zero = C*sum(DistSq_Zero)/Number;
-        
-        if (L_Zero < L_NotZ) * 0 % TODO 
-            alpha = alpha_Zero;
-            center = center_Zero;
-            radius = radius_Zero;
-            
-            model.in_idx  = find(DistSq_Zero <  radius);
-            model.on_idx  = find(DistSq_Zero == radius);
-            model.out_idx = find(DistSq_Zero >  radius);
-        else
-            alpha = alpha_NotZ;
-            center = center_NotZ;
-            radius = radius_NotZ;
-        end
+    m.SelfWise = zeros(Number,1); % count vector || x_i\cdot x_i ||
+    for i=1:Number
+        m.SelfWise(i) = kernel(Data(i,:), Data(i,:));
     end
     
-    model.alpha = alpha;
-
+    CoeffEq = ones(1,Number);
+    ValEq = 1;
+    lowerBound =       zeros(Number,1);   % \alpha_i >= 0
+    upperBound = m.C * ones(Number,1);    % \alpha_i <= C
+    opts = optimset('Algorithm','active-set','Display','off','TolX',1.e-15);
+    m.alpha = quadprog(2*m.PairWise,-m.SelfWise,[],[],CoeffEq,ValEq,lowerBound,upperBound,[],opts);
+    
+    m.in_idx = find(abs(m.alpha) < epsilon);
+    m.on_idx = find((m.alpha >= epsilon) & (m.alpha <= m.C - epsilon));
+    m.out_idx = find(m.alpha > m.C - epsilon);
     % form points ON, IN and OUT of sphere
-    model.in = Data(model.in_idx,:);
-    model.on = Data(model.on_idx,:);
-    model.out = Data(model.out_idx,:);
-
-    % form sphere parameters
-    model.center = center;
-    model.radius = radius;
+    m.in = m.Data(m.in_idx,:);
+    m.on = m.Data(m.on_idx,:);
+    m.out = m.Data(m.out_idx,:); 
     
-    % object is a row
-    model.classify = @(x) ((model.kernel(x,x) - 2*model.kernel(x, model.Data)*model.alpha + model.alpha'*model.PairWise*model.alpha) <= model.radius); 
+    if isempty(m.on)
+        error('ERROR: No points on sphere. Go debugging! Good luck!')
+    end
+    
+    m.count_sq_dist_t = @(x) (m.kernel(x,x) - 2*m.kernel(x, m.Data)*m.alpha + m.alpha'*m.PairWise*m.alpha);
+    m.count_sq_dist = @(X) (cellfun(m.count_sq_dist_t, num2cell(X,2)));
+    
+    m.radius = mean(sqrt(m.count_sq_dist(m.on)));
+    m.classify = @(x) (m.count_sq_dist(x) <= m.radius^2); 
 end
 
