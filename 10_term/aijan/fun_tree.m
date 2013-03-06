ac57a397d18852661a3495ba591c9e0a3c3f77d7
burmisha <burmisha@gmail.com>
2013-03-07 01:12:07 +0400

    add comments, fix bugs

--- 10_term/aijan/fun_tree.m
+++ 10_term/aijan/fun_tree.m
@@ -1,30 +1,29 @@
-function list = fun_tree(A)
-  % vxod A - matrisa ident,  vyxod spisok vershin
-  NVer = size(A, 1);
+function [list] = fun_tree(A)
+% input:  A - matrix: each column is one edge, which has 1 at its vertecies
+% output: [list] for Breadth-first search
+% Aijan Ibraimova, 2013 March
 
-  B = zeros(NVer);
-  idx = 1:NVer;
-  for e = 1:size(A, 2)
-        vertex = idx(A(:,e)==1);
-        B(vertex(1), vertex(2)) = 1;
-        B(vertex(2), vertex(1)) = 1;
-  end
- 
-  looked = zeros(NVer, 1); 
-  root = 1;
-  list = [root]; % final result
-  queue = [root];
-  looked(root) = 1;
-  
-  while ~isempty(queue)
-        vertex = queue(1);
-        incedent_vertecies = idx((B(:,vertex) == 1) & (looked == 0));
-        for i = 1:length(incedent_vertecies)
-            v = incedent_vertecies(i)
-            list = [list; v];
-            looked(v) = 1;
-            queue = [queue; v];
-        end
-        queue = queue(2:end)
-  end
+    NVer = size(A, 1);  % Number of vertecies in graph
+
+    B = zeros(NVer);    % prepare empty Adjacency matrix
+    idx = 1:NVer;       % useful vector ;-)
+    for e = 1:size(A, 2)                % take every edge
+        vertecies = idx(A(:,e)==1);        % find its's vertecies
+        B(vertecies(1), vertecies(2)) = 1;    % put this into 2 cells
+        B(vertecies(2), vertecies(1)) = 1;  
+    end
+
+    looked = zeros(NVer, 1);    % we've added this vertcies to the list
+    root = 1;                   % start vertex
+    list = [root];              % final result
+    queue = [root];             % start BFS from the root
+
+    while ~isempty(queue)       % while the queue isn't empty
+        vertex = queue(1);      % take it's first element
+        incedent_vertecies_bool = (B(:,vertex) == 1) & (looked == 0);   % find adjacent vertecies in bool format
+        list = [list, idx(incedent_vertecies_bool)];                    % add this vertecies to the list
+        queue = [queue, idx(incedent_vertecies_bool)];                  % and to the queue
+        queue = queue(2:end);
+        looked(vertex) = 1;     % this vertex will be forgotten
+    end
 end
