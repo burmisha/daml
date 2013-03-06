479cab4c31b9e67f4f4e5ec18fd7b1e726a3fdd5
burmisha <burmisha@gmail.com>
2013-03-07 00:57:26 +0400

    refactor

--- 10_term/aijan/fun_tree.m
+++ 10_term/aijan/fun_tree.m
@@ -1,36 +1,30 @@
-function Spis=fun_tree(A)
-% vxod A- matrisa ident,  vyxod spisok vershin
+function list = fun_tree(A)
+  % vxod A - matrisa ident,  vyxod spisok vershin
+  NVer = size(A, 1);
 
-kVer=size(A,1);   % kol strok
-kRebr=size(A,2);  % kol stolbsov
-
-VerInSpis=zeros(kVer);   % Obnulili massiv
-Spis=zeros(kVer);
-VerInSpis(1)=1;
-PorNomer=1
-Spis(PorNomer)=1 
-
-
-for i=1:kVer -1
-
-  for j=1:kRebr
-
-     if A(i,j)==1
-        for i1= i+1 : kVer
-          if (A(i1,j)==1) & (VerInSpis(i1)==0)
-            PorNomer=PorNomer+1
-            Spis(PorNomer)= i1
-            VerInSpis(i1)=1
-
-          end       
-
-
-        end
-
-     end
+  B = zeros(NVer);
+  idx = 1:NVer;
+  for e = 1:size(A, 2)
+        vertex = idx(A(:,e)==1);
+        B(vertex(1), vertex(2)) = 1;
+        B(vertex(2), vertex(1)) = 1;
   end
-
+ 
+  looked = zeros(NVer, 1); 
+  root = 1;
+  list = [root]; % final result
+  queue = [root];
+  looked(root) = 1;
+  
+  while ~isempty(queue)
+        vertex = queue(1);
+        incedent_vertecies = idx((B(:,vertex) == 1) & (looked == 0));
+        for i = 1:length(incedent_vertecies)
+            v = incedent_vertecies(i)
+            list = [list; v];
+            looked(v) = 1;
+            queue = [queue; v];
+        end
+        queue = queue(2:end)
+  end
 end
-
-
-return
